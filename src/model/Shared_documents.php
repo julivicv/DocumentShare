810c390ae08b6822078428a20b7cc62ce3dfc6c4
julivicv <102142918+julivicv@users.noreply.github.com>
2023-05-11 09:03:38 -0300

    Added the document sharing function

--- src/model/Shared_documents.php
+++ src/model/Shared_documents.php
@@ -2,4 +2,26 @@
 
 class Shared_documents extends Model
 {
+    public function shareDocument($uid, $email, $fid) {
+        $sql = $this->conex->prepare("SELECT * FROM `users` `u` LEFT JOIN `documents` `d` WHERE `u`.`id` = :uid AND `d`.`id` = :fid");
+        $sql->bindParam(':uid', $uid);
+        $sql->bindParam(':fid', $fid);
+        $sql->execute();
+
+        if($sql->rowCount() > 0) {
+            try {
+                $userShare = $this->conex->prepare("SELECT `id` FROM `users` `u``u`.`email` = :email");
+                $userShare->bindParam(':email', $email);
+                $userShare->execute();
+                $userId = $userShare->fetch(PDO::FETCH_ASSOC);
+                $sql = $this->conex->prepare("INSERT INTO {$this->table} (users_id, document_id) VALUES (:uid, :fid)");
+                $sql->bindParam(':uid', $userId['id']);
+                $sql->bindParam(':fid', $fid);
+                $sql->execute();
+                return ("Documento compartilhado com sucesso!");
+            } catch(err) {
+                return $err;
+            }
+        }
+    }
 }
