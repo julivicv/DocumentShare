90ae46c65f363bdb3b9480fcde56509121ee7fbe
julivicv <102142918+julivicv@users.noreply.github.com>
2023-05-11 10:09:15 -0300

    Added the document sharing function

--- src/model/Shared_documents.php
+++ src/model/Shared_documents.php
@@ -3,13 +3,12 @@
 class Shared_documents extends Model
 {
     public function shareDocument($uid, $email, $fid) {
-        $sql = $this->conex->prepare("SELECT * FROM `users` `u` LEFT JOIN `documents` `d` WHERE `u`.`id` = :uid AND `d`.`id` = :fid");
+        $sql = $this->conex->prepare("SELECT * FROM `users` `u` LEFT JOIN `documents` `d` ON `u`.`id` = `d`.`users_id` WHERE `u`.`id` = :uid AND `d`.`id` = :fid");
         $sql->bindParam(':uid', $uid);
         $sql->bindParam(':fid', $fid);
         $sql->execute();
 
         if($sql->rowCount() > 0) {
-            try {
                 $userShare = $this->conex->prepare("SELECT `id` FROM `users` `u``u`.`email` = :email");
                 $userShare->bindParam(':email', $email);
                 $userShare->execute();
@@ -19,9 +18,20 @@ class Shared_documents extends Model
                 $sql->bindParam(':fid', $fid);
                 $sql->execute();
                 return ("Documento compartilhado com sucesso!");
-            } catch(err) {
-                return $err;
-            }
         }
+    }
+
+    public function verifySharedDocument($uid, $fid) {
+        $sql = $this->conex->prepare("SELECT * FROM `documents` `d` LEFT JOIN `users` `u` on `u`.`id` = `d`.`users_id` LEFT JOIN `shared_documents` `sd` on `d`.`id` = `sd`.`document_id` WHERE `u`.`id` NOT NULL");
+        $sql->bindParam(':uid', $uid);
+        $sql->bindParam(':fid', $fid);
+        $sql->execute();
+        if($sql->rowCount() < 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
     }
 }
